99a58965f41095abf860bf12b1f0a2a2dd48502f
pandi.tamas93 <pandi.tamas93@gmail.com>
2015-05-06 00:48:10 +0200

    Auto KATEGORIEEEEEE YEAHHHHHHHHH Signed-off-by: pandi.tamas93 <pandi.tamas93@gmail.com>

--- csv.php
+++ csv.php
@@ -71,41 +71,35 @@ Field:</br> <textarea rows="15" cols="75" name="CSV"></textarea>
 			
 			if($currentrowarray[0]!='Tranzakció dátuma'){
 				$sql="SELECT KategorieName FROM Kategorie JOIN Autokategorie ON Kategorie.KategorieID = autokategorie.KategorieID 
-				WHERE "$currentrowarray[1]"  LIKE CONCAT('%', Zeichenkette, '%')";
-					$result = $mysqli->use_result();
-					if(mysql_num_rows($result)==0){
-						
+				WHERE '$currentrowarray[1]'  LIKE CONCAT('%', Zeichenkette, '%')";
+					$result=mysqli_query($mysqli,$sql);
+					$count=mysqli_num_rows($result);
+					echo $count;
+					if($count!=0){
+						$KategName = array();
+								while ( $row = $result->fetch_row() ) {
+			
+									$KategName[]= $row;
+								}
+						//echo $KategName[0][0];
+						$query= sprintf("INSERT INTO transaktionen (KategorieName,TransaktionsDatum, BenutzerName, BenutzerKonto, KontoName, KontoNummer,
+				TransaktionsTyp, TransaktionsSumme, TransaktionsDevisen, TransaktionsUSumme, TransaktionsUDevisen, EinnameoderAusgabe,
+				KartenNummer, Wertstellung, Mitteilung, Daten, KontoStand, DevisenTyp)  VALUES('%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'
+				,'%s','%s','%s','%s','%s','%s','%s')",$KategName[0][0],$currentrowarray[0], $currentrowarray[1],$currentrowarray[2],$currentrowarray[3],$currentrowarray[4],
+				$currentrowarray[5],$currentrowarray[6],$currentrowarray[7],$currentrowarray[8],$currentrowarray[9],$currentrowarray[10],
+				$currentrowarray[11],$currentrowarray[12],$currentrowarray[13],$currentrowarray[14],$currentrowarray[15],$currentrowarray[16]); 
+				$mysqli->query($query);
+					}
+					else{
 						$query= sprintf("INSERT INTO transaktionen (TransaktionsDatum, BenutzerName, BenutzerKonto, KontoName, KontoNummer,
 				TransaktionsTyp, TransaktionsSumme, TransaktionsDevisen, TransaktionsUSumme, TransaktionsUDevisen, EinnameoderAusgabe,
 				KartenNummer, Wertstellung, Mitteilung, Daten, KontoStand, DevisenTyp)  VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'
 				,'%s','%s','%s','%s','%s','%s','%s')",$currentrowarray[0], $currentrowarray[1],$currentrowarray[2],$currentrowarray[3],$currentrowarray[4],
 				$currentrowarray[5],$currentrowarray[6],$currentrowarray[7],$currentrowarray[8],$currentrowarray[9],$currentrowarray[10],
 				$currentrowarray[11],$currentrowarray[12],$currentrowarray[13],$currentrowarray[14],$currentrowarray[15],$currentrowarray[16]); 
-				$mysqli->query($query); 
+				$mysqli->query($query);
 					}
-					else{
-							$kategorie = array();
-								while ( $row = $result->fetch_row() ) {
-			
-									$KategName[]= $row;
-								}
-								$result->close();
-								$KategName2=$KategName[0][0];
-								echo("adat");
-					
-						//////echo $kategorie[0][0];
-						$query= sprintf("INSERT INTO transaktionen (KategorieName,TransaktionsDatum, BenutzerName, BenutzerKonto, KontoName, KontoNummer,
-				TransaktionsTyp, TransaktionsSumme, TransaktionsDevisen, TransaktionsUSumme, TransaktionsUDevisen, EinnameoderAusgabe,
-				KartenNummer, Wertstellung, Mitteilung, Daten, KontoStand, DevisenTyp)  VALUES('%s', '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s'
-				,'%s','%s','%s','%s','%s','%s','%s')",$KategName2,$currentrowarray[0], $currentrowarray[1],$currentrowarray[2],$currentrowarray[3],$currentrowarray[4],
-				$currentrowarray[5],$currentrowarray[6],$currentrowarray[7],$currentrowarray[8],$currentrowarray[9],$currentrowarray[10],
-				$currentrowarray[11],$currentrowarray[12],$currentrowarray[13],$currentrowarray[14],$currentrowarray[15],$currentrowarray[16]); 
-				$mysqli->query($query); 
-				
-					}
-				
-				
-				
+							
 				
 	}
 			}
